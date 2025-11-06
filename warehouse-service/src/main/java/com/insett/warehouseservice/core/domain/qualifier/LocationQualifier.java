36bf58ff577d30a6be45b0184a38e848ec0c14e4
szytambsky <szytamborski@gmail.com>
2025-11-07 00:05:07 +0100

    extend enums usability

--- warehouse-service/src/main/java/com/insett/warehouseservice/core/domain/qualifier/LocationQualifier.java
+++ warehouse-service/src/main/java/com/insett/warehouseservice/core/domain/qualifier/LocationQualifier.java
@@ -1,7 +1,20 @@
 package com.insett.warehouseservice.core.domain.qualifier;
 
+import lombok.Getter;
+
+@Getter
 public enum LocationQualifier {
-    WARSAW,
-    TOKYO,
-    FRANKFURT
+    WARSAW("Warsaw", "Poland", "WAW"),
+    TOKYO("Tokyo", "Japan", "TYO"),
+    FRANKFURT("Frankfurt", "Germany", "FRA");
+
+    private final String cityName;
+    private final String country;
+    private final String zipCode;
+
+    LocationQualifier(String cityName, String country, String zipCode) {
+        this.cityName = cityName;
+        this.country = country;
+        this.zipCode = zipCode;
+    }
 }
--- warehouse-service/src/main/java/com/insett/warehouseservice/core/domain/qualifier/ProductCategoryType.java
+++ warehouse-service/src/main/java/com/insett/warehouseservice/core/domain/qualifier/ProductCategoryType.java
@@ -1,13 +1,33 @@
 package com.insett.warehouseservice.core.domain.qualifier;
 
+import lombok.Getter;
+
+@Getter
 public enum ProductCategoryType {
-    BUSINESS,
-    REAL_ESTATE,
-    ELECTRONICS,
-    FASHION,
-    SPORTS,
-    MUSIC,
-    COLLECTIBLES,
-    HEALTH,
-    FOR_CHILDREN
+    BUSINESS(15),
+    REAL_ESTATE(22),
+    ELECTRONICS(30),
+    FASHION(30),
+    SPORTS(40),
+    MUSIC(12),
+    COLLECTIBLES(22),
+    HEALTH(10),
+    FOR_CHILDREN(15);
+
+    ProductCategoryType(int value) {
+        this.taxRate = value;
+    }
+
+    private final int taxRate;
+
+    public boolean isForAdults() {
+        return !this.equals(ProductCategoryType.FOR_CHILDREN);
+    }
+
+    public boolean isPhysicalProduct() {
+        return switch(this) {
+            case REAL_ESTATE, ELECTRONICS, FASHION, SPORTS, COLLECTIBLES, FOR_CHILDREN -> true;
+            default -> false;
+        };
+    }
 }
