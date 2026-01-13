fe9fc0845165b5d315ca08506a61ec304e90be12
szytambsky <szytamborski@gmail.com>
2026-01-13 16:51:00 +0100

    feature(elk): add test with bulk api

--- indices-service/src/test/java/com/insett/indicesservice/domain/dao/ProductRepository.java
+++ indices-service/src/test/java/com/insett/indicesservice/domain/dao/ProductRepository.java
@@ -4,9 +4,11 @@ import com.insett.indicesservice.entity.Product;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends ElasticsearchRepository<Product, String> {
     Optional<Product> findProductByTitleAndBrand(String title, String brand);
+    List<Product> findAllByTitleAndCategory(String title, String brand);
 }
