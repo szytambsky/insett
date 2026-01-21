9c31577b6390e02573c305336b1d95524312a6ed
szytambsky <szytamborski@gmail.com>
2026-01-21 16:57:18 +0100

    feature(elk): order test suite template

---
+++ indices-service/src/test/java/com/insett/indicesservice/entity/OrderItemList.java
@@ -0,0 +1,14 @@
+package com.insett.indicesservice.entity;
+
+import java.math.BigDecimal;
+
+public class OrderItemList {
+    private String orderItemId;
+    private String orderId;
+    private String productId;
+    private String listingId;
+    private BigDecimal quantity;
+    private BigDecimal purchasePrice;
+    private String sku;
+    private String OrderItemStatus; //(PENDING, RESERVED, FULFILLED, CANCELLED)
+}
